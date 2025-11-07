8b0a12b9520e986949ae4e5392941e16c063d76e
haleyannrobinson2-web <haleyannrobinson2@gmail.com>
2025-11-07 12:39:31 +0100

    help

--- week 4/week4_exercsise1.py
+++ week 4/week4_exercsise1.py
@@ -12,4 +12,8 @@ failed_counts = {}
 print ("Checking login attempts...")
 
 
-
+for username, status in login_attempts:
+    if status == "failed"
+        if username in failed_counts:
+            failed_counts[username] = failed_counts[username] + 1
+        else: 
